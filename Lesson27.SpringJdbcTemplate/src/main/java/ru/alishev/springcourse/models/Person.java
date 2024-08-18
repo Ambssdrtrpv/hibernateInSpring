506727df5fec94cb80a6cc4d0618a50b99e81442
”Sabina <sabina.elchueva@gmail.com>
2024-08-18 16:31:20 +0300

    methods in JPA

--- Lesson27.SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/models/Person.java
+++ Lesson27.SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import javax.xml.crypto.Data;
+import java.util.List;
 
 @Entity
 @Table(name = "Person")
@@ -25,6 +27,14 @@ public class Person {
     @Min(value = 0, message = "Age should be greater than 0")
     @Column(name = "age")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Column(name = "email")
+    @Email
+    private String email;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person() {}
 
@@ -56,5 +66,12 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-//
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
